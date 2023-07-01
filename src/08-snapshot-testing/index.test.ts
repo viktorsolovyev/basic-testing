2f0d1a1ee14c1aa029930bdda50ebdda83e92035
Viktor Solovyev <barn-mih@rambler.ru>
2023-07-02 00:13:38 +0300

    feat: add 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,15 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
+  const linkedList = {
+    value: 1,
+    next: { value: 2, next: { value: 3, next: { value: null, next: null } } },
+  };
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3])).toStrictEqual(linkedList);
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList([1, 2, 3])).toMatchSnapshot();
   });
 });
